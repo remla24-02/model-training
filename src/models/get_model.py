edc5bd2e0134151c7a97c4a74838285b9ef0feae
Rodin Haker <rodinhaker@hotmail.com>
2024-06-27 22:40:13 +0200

    Revert to boto3

--- src/models/get_model.py
+++ src/models/get_model.py
@@ -1,37 +1,44 @@
-"""
-Download and extract training, testing and validation data.
-"""
-
 import os
-import subprocess
-import sys
+import yaml
+import boto3
+from botocore import UNSIGNED
+from botocore.config import Config
 
 
-def pull_specific_files(output_file):
+def download_data(bucket_name, file_name, output_file):
     """
     Pull specific files from the DVC remote storage.
     """
-    try:
-        dvc_repo_url = os.path.abspath('.')
-        subprocess.run(["dvc", "get", dvc_repo_url, "models/trained_model.joblib",
-                       "-o", output_file, "--force"], check=True)
-        print("Successfully pulled the latest model.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error pulling the latest model: {e}")
-        sys.exit(1)
+    s3 = boto3.client('s3', region_name='eu-north-1',
+                      config=Config(signature_version=UNSIGNED))
+    s3.download_file(bucket_name, file_name, output_file)
 
 
-def main(output_file: str = 'models/trained_model.joblib'):
+def main(model_name: str = 'trained_model'):
     """
-    Main function to ensure the raw data directory exists and pull the latest data.
+    Main function to download the specific trained model from dvc.lock.
     """
-    model_dir = os.path.join('models')
+    bucket_name = 'dvc-remla24-02'
 
-    # Ensure the raw data directory exists
-    os.makedirs(model_dir, exist_ok=True)
+    # Read the dvc.lock file
+    with open('dvc.lock', 'r', encoding='utf-8') as file:
+        dvc_lock_data = yaml.safe_load(file)
 
-    # Pull the specified files
-    pull_specific_files(output_file)
+    # Find the path and md5 hash for models/trained_model.joblib
+    for stage in dvc_lock_data['stages'].values():
+        for out in stage.get('outs', []):
+            if out['path'] == 'models/trained_model.joblib':
+                md5_hash = out['md5']
+                key = f'data/files/md5/{md5_hash[:2]}/{md5_hash[2:]}'
+                output_file = os.path.join('models', f'{model_name}.joblib')
+
+                # Ensure directory exists
+                os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+                download_data(bucket_name, key, output_file)
+                return
+
+    print("The specified model path 'models/trained_model.joblib' was not found in dvc.lock")
 
 
 if __name__ == '__main__':
